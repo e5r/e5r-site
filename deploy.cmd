2b3841a448406d24caca3ec36b4c9f10472e0172
Erlimar Silva Campos <erlimar@gmail.com>
2016-01-07 13:26:09 -0200

    Change deploy script and project and host configuration to identify environment and iis web command

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,34 @@
 @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
 
+:: ---------------------------------
+:: Script only runs in Windows Azure
+:: ---------------------------------
+IF NOT DEFINED WEBSITE_HOSTNAME (
+    echo Script only runs in Windows Azure.
+    goto error
+)
+
+:: --------------------------
+:: In Production Environment?
+:: --------------------------
+echo.
+echo Detecting type of environment.
+echo.
+IF "%WEBSITE_HOSTNAME%" == "%WEBSITE_SITE_NAME%.azurewebsites.net" (
+    set SCM_DNU_PUBLISH_IISCOMMAND=web
+    echo ^> Production environment
+) else (
+    set SCM_DNU_PUBLISH_IISCOMMAND=web-staging
+    echo ^> Staging environment
+)
+echo.
+
 :: ----------------------
 :: Site Deploy DNX Config
 :: ----------------------
 set SITE_PROJECT_PATH=src\E5R.Product.WebSite\project.json
 set SCM_DNU_RESTORE_OPTIONS=--no-cache
-set SCM_DNU_PUBLISH_OPTIONS=--no-source --configuration Release
+set SCM_DNU_PUBLISH_OPTIONS=--no-source --configuration Release --iis-command %SCM_DNU_PUBLISH_IISCOMMAND%
 set SCM_DNX_VERSION=1.0.0-rc1-final
 set SCM_DNX_ARCH=x86
 set SCM_DNX_RUNTIME=CLR
@@ -22,8 +45,8 @@ set SCM_DNVM_INSTALL_OPTIONS=-arch %SCM_DNX_ARCH% -r %SCM_DNX_RUNTIME%
 :: Verify node.js installed
 where node 2>nul >nul
 IF %ERRORLEVEL% NEQ 0 (
-  echo Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment.
-  goto error
+    echo Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment.
+    goto error
 )
 
 :: Setup
@@ -34,38 +57,38 @@ setlocal enabledelayedexpansion
 SET ARTIFACTS=%~dp0%..\artifacts
 
 IF NOT DEFINED DEPLOYMENT_SOURCE (
-  SET DEPLOYMENT_SOURCE=%~dp0%.
+    SET DEPLOYMENT_SOURCE=%~dp0%.
 )
 
 IF NOT DEFINED DEPLOYMENT_TARGET (
-  SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot
+    SET DEPLOYMENT_TARGET=%ARTIFACTS%\wwwroot
 )
 
 IF NOT DEFINED NEXT_MANIFEST_PATH (
-  SET NEXT_MANIFEST_PATH=%ARTIFACTS%\manifest
+    SET NEXT_MANIFEST_PATH=%ARTIFACTS%\manifest
 
-  IF NOT DEFINED PREVIOUS_MANIFEST_PATH (
-    SET PREVIOUS_MANIFEST_PATH=%ARTIFACTS%\manifest
-  )
+    IF NOT DEFINED PREVIOUS_MANIFEST_PATH (
+        SET PREVIOUS_MANIFEST_PATH=%ARTIFACTS%\manifest
+    )
 )
 
 IF NOT DEFINED KUDU_SYNC_CMD (
-  :: Install kudu sync
-  echo Installing Kudu Sync
-  call npm install kudusync -g --silent
-  IF !ERRORLEVEL! NEQ 0 goto error
+    :: Install kudu sync
+    echo Installing Kudu Sync
+    call npm install kudusync -g --silent
+    IF !ERRORLEVEL! NEQ 0 goto error
 
-  :: Locally just running "kuduSync" would also work
-  SET KUDU_SYNC_CMD=%appdata%\npm\kuduSync.cmd
+    :: Locally just running "kuduSync" would also work
+    SET KUDU_SYNC_CMD=%appdata%\npm\kuduSync.cmd
 )
 IF NOT DEFINED DEPLOYMENT_TEMP (
-  SET DEPLOYMENT_TEMP=%temp%\___deployTemp%random%
-  SET CLEAN_LOCAL_DEPLOYMENT_TEMP=true
+    SET DEPLOYMENT_TEMP=%temp%\___deployTemp%random%
+    SET CLEAN_LOCAL_DEPLOYMENT_TEMP=true
 )
 
 IF DEFINED CLEAN_LOCAL_DEPLOYMENT_TEMP (
-  IF EXIST "%DEPLOYMENT_TEMP%" rd /s /q "%DEPLOYMENT_TEMP%"
-  mkdir "%DEPLOYMENT_TEMP%"
+    IF EXIST "%DEPLOYMENT_TEMP%" rd /s /q "%DEPLOYMENT_TEMP%"
+    mkdir "%DEPLOYMENT_TEMP%"
 )
 
 IF DEFINED MSBUILD_PATH goto MsbuildPathDefined
