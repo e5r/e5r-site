edece6b5cba587a1d20a876f35e40b2af49f3514
Erlimar Silva Campos <erlimar@gmail.com>
2016-01-12 15:22:09 -0200

    Prepare SignUp controller

--- src/E5R.Product.WebSite/Controllers/Account.cs
+++ src/E5R.Product.WebSite/Controllers/Account.cs
@@ -25,11 +25,9 @@ namespace E5R.Product.WebSite.Controllers
         
         [HttpGet]
         [AllowAnonymous]
-        public async Task<IActionResult> SignIn(string urlReturn = null)
+        public IActionResult SignIn(string urlReturn = null)
         {
             ViewBag.UrlReturn = urlReturn;
-            
-            await SignInManager.SignOutAsync();
             
             return View(new SignInViewModel { RememberMe = true });
         }
@@ -39,6 +37,8 @@ namespace E5R.Product.WebSite.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> SignIn(SignInViewModel model, string urlReturn = null)
         {
+            await SignInManager.SignOutAsync();
+            
             ViewBag.UrlReturn = urlReturn;
 
             if (ModelState.IsValid)
@@ -81,5 +81,59 @@ namespace E5R.Product.WebSite.Controllers
             
             return RedirectToAction("index", "home");
         }
+        
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignUp()
+        {
+            // Send mail with Mailgun
+            // * https://mailgun.com
+            // * https://documentation.mailgun.com/quickstart-sending.html#send-via-api
+        }
+        
+        // public static IRestResponse SendSimpleMessage()
+        // {
+        //     RestClient client = new RestClient();
+        //     client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+        //     client.Authenticator =
+        //             new HttpBasicAuthenticator("api",
+        //                                        "YOUR_API_KEY");
+        //     RestRequest request = new RestRequest();
+        //     request.AddParameter("domain",
+        //                          "YOUR_DOMAIN_NAME", ParameterType.UrlSegment);
+        //     request.Resource = "{domain}/messages";
+        //     request.AddParameter("from", "Excited User <mailgun@YOUR_DOMAIN_NAME>");
+        //     request.AddParameter("to", "bar@example.com");
+        //     request.AddParameter("to", "YOU@YOUR_DOMAIN_NAME");
+        //     request.AddParameter("subject", "Hello");
+        //     request.AddParameter("text", "Testing some Mailgun awesomness!");
+        //     request.Method = Method.POST;
+        //     return client.Execute(request);
+        //     
+        //     /*
+        //     using System.Net.Http;
+        //     using Microsoft.AspNet.Http; 
+        //     
+        //     HttpClient client = new HttpClient();
+        //     */
+        //     
+        //     /*
+        //     curl -s --user 'api:YOUR_API_KEY' \
+        //         https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages \
+        //         -F from='Excited User <mailgun@YOUR_DOMAIN_NAME>' \
+        //         -F to=YOU@YOUR_DOMAIN_NAME \
+        //         -F to=bar@example.com \
+        //         -F subject='Hello' \
+        //         -F text='Testing some Mailgun awesomness!'
+        //      */
+        // }
     }
 }
